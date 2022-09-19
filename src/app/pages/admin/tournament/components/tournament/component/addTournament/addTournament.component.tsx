7ce8ce98f9e42a44b89565b63183bde24a3c4781
ankit417 <acharya.ankit417@gmail.com>
2022-09-19 10:27:05 +0545

    Add tournament modal

--- src/app/pages/admin/tournament/components/tournament/component/addTournament/addTournament.component.tsx
+++ src/app/pages/admin/tournament/components/tournament/component/addTournament/addTournament.component.tsx
@@ -1,8 +1,97 @@
-const AddTournament = () => {
+import { Modal } from 'react-uicomp'
+import { useFormInput } from 'use-form-input'
+import { AiFillCloseCircle } from 'react-icons/ai'
+import {
+  Box,
+  Button,
+  FormInput,
+  Hrline,
+  InputField,
+  Title,
+} from '../../../../../../../common'
+
+const AddTournament = ({ visible, onClose, onSubmit }: any) => {
+  const [data, { onChange, clear }] = useFormInput({
+    tournament_name: '',
+    start_date: '',
+    end_date: '',
+    prediction_deadline: '',
+  })
+
+  const onSubmitHandler = (e: any) => {
+    e.preventDefault()
+    const { tournament_name, start_date, end_date, prediction_deadline } = data
+    const requestBody = {
+      tournament_name,
+      start_date,
+      end_date,
+      prediction_deadline,
+    }
+    onSubmit(requestBody)
+    clear()
+  }
+
   return (
-    <div>
-      <div>Add Tournament</div>
-    </div>
+    <Modal visible={visible}>
+      <div>
+        <div className="addTournament-title-wrapper">
+          <Title>Add Tournament</Title>
+          <AiFillCloseCircle
+            size={24}
+            className="addTournament-close-button"
+            onClick={onClose}
+          />
+        </div>
+        <Hrline />
+        <div>
+          <form onSubmit={onSubmitHandler}>
+            <Box flexBox vertical columnGap={20}>
+              <FormInput label="Tournament Name" required>
+                <InputField
+                  placeholder="Tournament Name"
+                  name="tournament_name"
+                  value={data.tournament_name}
+                  onChange={onChange}
+                  type="text"
+                  required
+                />
+              </FormInput>
+              <FormInput label="Start Date" required>
+                <InputField
+                  placeholder="Start Date"
+                  name="start_date"
+                  value={data.start_date}
+                  onChange={onChange}
+                  type="date"
+                  required
+                />
+              </FormInput>
+              <FormInput label="End Date" required>
+                <InputField
+                  placeholder="End Date"
+                  name="end_date"
+                  value={data.end_date}
+                  onChange={onChange}
+                  type="date"
+                  required
+                />
+              </FormInput>
+              <FormInput label="Prediction Deadline" required>
+                <InputField
+                  placeholder="Prediction Deadline"
+                  name="prediction_deadline"
+                  value={data.prediction_deadline}
+                  onChange={onChange}
+                  type="date"
+                  required
+                />
+              </FormInput>
+              <Button title="Submit" type="submit" />
+            </Box>
+          </form>
+        </div>
+      </div>
+    </Modal>
   )
 }
 
--- src/app/pages/admin/tournament/components/tournament/tournament.component.tsx
+++ src/app/pages/admin/tournament/components/tournament/tournament.component.tsx
@@ -1,13 +1,15 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import { useDispatch, useSelector } from 'react-redux'
 // import { useNavigation } from 'react-auth-navigation'
 
 import { BsPlusCircleFill } from 'react-icons/bs'
+import { AddTournament } from './component'
 import { getAllTournaments, RootState } from '../../../../../../redux'
 import { Hrline, Title } from '../../../../../common'
 
 const TournamentList = ({ onPress }: any) => {
+  const [addTournamentVisible, setAddTournamentModal] = useState<boolean>(false)
   // const {
   //   navigation: { navigate },
   //   // history,
@@ -35,15 +37,32 @@ const TournamentList = ({ onPress }: any) => {
     onPress(tournamentId)
   }
 
+  const handleAddTournamentModal = () => {
+    setAddTournamentModal((prev) => !prev)
+  }
+
+  const onAddTournament = (requestBody: any) => {
+    console.log('request body', requestBody)
+  }
+
   return (
     <div>
       <div>
         <div className="tournament-title-wrapper">
           <Title>Tournaments</Title>
-          <BsPlusCircleFill size={24} className="add-tournament" />
+          <BsPlusCircleFill
+            size={24}
+            className="add-tournament"
+            onClick={handleAddTournamentModal}
+            onSubmit={onAddTournament}
+          />
         </div>
         <Hrline />
       </div>
+      <AddTournament
+        visible={addTournamentVisible}
+        onClose={handleAddTournamentModal}
+      />
       {tournamentList.map((item: any, index: number) => {
         return (
           <div
